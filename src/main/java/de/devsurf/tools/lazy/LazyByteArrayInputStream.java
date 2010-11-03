a61074f44e4613ab4cdd36be9bc78e734358d0ce
Daniel Manzke <daniel.manzke@googlemail.com>
2010-11-04 00:21:36 +0100

    added license headers

--- src/main/java/de/devsurf/tools/lazy/LazyByteArrayInputStream.java
+++ src/main/java/de/devsurf/tools/lazy/LazyByteArrayInputStream.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010 Daniel Manzke <daniel.manzke@saperion.com>
+ * Copyright (C) 2010 Daniel Manzke <daniel.manzke@googlemail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
